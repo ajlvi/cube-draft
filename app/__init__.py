45102d44bde0cd16bfea36ba7f962db8e70497f9
bekka <ajolipa@gmail.com>
2020-05-27 17:57:42 -0400

    trying to give ada access

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 app = Flask(__name__)
 #app.config.from_object(Config)
-a#pp.config["DEBUG"] = True
+app.config["DEBUG"] = True
 #db = SQLAlchemy(app)
 #migrate = Migrate(app, db)
 
