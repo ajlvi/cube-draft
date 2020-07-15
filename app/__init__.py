1a51602cc081dbb9fc12647199d7b8aafe91167d
bekka <ajolipa@gmail.com>
2020-07-15 16:05:59 -0400

    working on redis url

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
 from flask_redis import FlaskRedis
+import os
+import redis 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config["DEBUG"] = True
-redis_client = FlaskRedis(app)
+redis_client = redis.from_url(os.environ.get("REDIS_URL"))
 
 from app import routes
--- config.py
+++ config.py
@@ -3,4 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-	REDIS_URL = "redis://h:p18ca4e75cfaba7dea7028f639f54e15682117493b6cd9a2982ec2ed2217e751b@ec2-52-70-215-224.compute-1.amazonaws.com:29529"
+	#REDIS_URL = "redis://h:p18ca4e75cfaba7dea7028f639f54e15682117493b6cd9a2982ec2ed2217e751b@ec2-52-70-215-224.compute-1.amazonaws.com:29529"
