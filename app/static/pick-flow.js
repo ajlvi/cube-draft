8da2874ec7ab2835d9ec6451a0d84b72dc8ba89f
ajlvi <ajlvii@gmail.com>
2020-08-07 18:42:25 -0400

    Time container (part two)

--- app/static/pick-flow.js
+++ app/static/pick-flow.js
@@ -75,7 +75,7 @@ function runTimer(num) {
 		sec = num - min*60;
 		if (sec < 10) {secstring = '0'+ sec.toString();}
 		else {secstring = sec.toString();}
-		$("#timer").text("<span class='timebox'>" + min.toString() + ':' + secstring + "</span>");
+		$("#timer").html("<span class='timebox'>" + min.toString() + ':' + secstring + "</span>");
 		timeFloor--;
 	}
 	else if (num==0) {
